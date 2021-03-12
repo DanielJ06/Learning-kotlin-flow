83279ead4f255aba240d5c845ffd5929a0e33c82
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-03-11 21:41:58 -0300

    Setup viewModel

--- app/src/main/java/com/example/kotlinflowtutorial/MainViewModel.kt
+++ app/src/main/java/com/example/kotlinflowtutorial/MainViewModel.kt
@@ -2,12 +2,30 @@ package com.example.kotlinflowtutorial
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class MainViewModel : ViewModel() {
 
     lateinit var languagesData: LiveData<String>
 
-    fun getLanguages() {
+    fun getLanguages(): Flow<String> {
+        return flow {
+            val languages = listOf(
+                "Kotlin",
+                "Javascript",
+                "Dart",
+                "GO",
+                "Elixir",
+                "Swift",
+                "PHP"
+            )
 
+            for (language in languages) {
+                emit(language)
+                delay(1000L)
+            }
+        }
     }
 }
